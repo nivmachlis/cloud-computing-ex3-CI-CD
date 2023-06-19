9c90291d825794d9ebbbcb0b419cd0dcbdc6f7b7
nivmachlis <nivmachliss@gmail.com>
2023-06-19 22:23:31 +0300

    test to finish

--- jobProcess.py
+++ jobProcess.py
@@ -6,19 +6,25 @@ with open("query.txt", "r") as query_file:
 response_lines = []
 for dish in query_lines:
     try:
+        print(f"{dish}")
         response_post = requests.post("http://localhost:8000/dishes", json={"name": dish},headers={"Content-Type": "application/json"})
+        print(f"response post status: {response_post.status_code}")
         if response_post.status_code == 201:
             response_get = requests.get(f"http://localhost:8000/dishes/{dish}",headers={"Content-Type": "application/json"})
+            print(f"response get status: {response_get.status_code}")
             if response_get.status_code == 200:
                 data = response_get.json()
+                print(f"data for {dish}:{data}")
                 calories = data["cal"]
                 sodium = data["sodium"]
                 sugar = data["sugar"]
                 
                 response_lines.append(f"{dish} contains {calories} calories, {sodium} mgs of sodium, and {sugar} grams of sugar")
             else:
+                print(f"get failed beacuse {response_get.status_code}")
                 raise Exception()
         else:
+            print(f"post failed beacuse {response_post.status_code}")
             raise Exception()        
     except:
         response_lines.append(f"Error: Failed to retrieve data for {dish}")
